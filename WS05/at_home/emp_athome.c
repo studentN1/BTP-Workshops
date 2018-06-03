609da81aa04e9697d31eaac63bcd868a1cb82cbc
Cameron Gray <cfgray72@gmail.com>
2018-06-03 18:53:55 -0400

    Workshop #5 - Released

---
+++ WS05/at_home/emp_athome.c
@@ -0,0 +1,346 @@
+/*
+Name:
+Student number:
+Email:
+Workshop:
+Section:
+Date:
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+#define SIZE 4			// Number of Employees
+
+// Structure Declaration for Employee Data
+struct Employee {
+	int empID;			// Employee ID
+	int age;			// Employee Age
+	double salary;		// Employee Yearly Salary 
+};
+
+/* main program */
+int main(void) {
+
+	int option = 0, check = -1;
+	int empNo = 0, tempEmpID;
+   int i;
+   struct Employee emp[SIZE] = { {0} };
+
+	printf("---=== EMPLOYEE DATA ===---\n\n");
+
+	do {
+	
+		// Print the option list
+		printf("1. Display Employee Information\n");
+		printf("2. Add Employee\n");
+		printf("3. Update Employee Salary\n");
+		printf("4. Remove Employee\n");
+		printf("0. Exit\n\n");
+		printf("Please select from the above options: ");
+		scanf("%d", &option);
+		printf("\n");
+
+		switch (option) {
+		case 0: // Exit the program
+			printf("Exiting Employee Data Program. Good Bye!!!\n");
+			break;
+
+		case 1:	// Display Employee Data
+				// @IN-LAB
+			printf("EMP ID  EMP AGE EMP SALARY\n");
+			printf("======  ======= ==========\n");
+			for (i = 0; i < SIZE; i++) {
+				if (emp[i].empID > 0)
+					printf("%6d%9d%11.2lf\n", emp[i].empID, emp[i].age, emp[i].salary);
+			}
+			printf("\n");
+			break;
+
+		case 2: // Adding Employee
+				// @IN-LAB
+				
+			printf("Adding Employee\n");
+			printf("===============\n");
+
+			if (empNo >= SIZE) {
+			
+				printf("ERROR!!! Maximum Number of Employees Reached\n");
+
+			}
+			else 
+			{
+				printf("Enter Employee ID: ");
+				scanf("%d", &tempEmpID);
+				emp[empNo].empID = tempEmpID;
+				printf("Enter Employee Age: ");
+				scanf("%d", &emp[empNo].age);
+				printf("Enter Employee Salary: ");
+				scanf("%lf", &emp[empNo].salary);
+				empNo++;
+				
+			}
+		
+			printf("\n");
+
+			break;
+
+		case 3: // Update Employee Data
+				// @AT-HOME
+			printf("Update Employee Salary\n");
+			printf("======================\n");
+
+			do {
+
+				printf("Enter Employee ID: ");
+				scanf("%d", &tempEmpID);
+
+				if (tempEmpID <= 0) {
+					printf("ERROR: EmployeeID should be greater than zero !!\n");
+				}
+
+			} while (tempEmpID <= 0);
+
+			//Find the record
+			for (i = 0; i < SIZE && check == -1; i++) {
+				if (emp[i].empID == tempEmpID) {
+					check = i;
+				}
+			}
+
+			// Employee Found
+			if (check >= 0) {
+				
+				printf("The current salary is %0.2lf\n", emp[check].salary);
+				printf("Enter Employee New Salary: ");
+				scanf("%lf", &emp[check].salary);
+				printf("\n");
+				check = -1;
+			}
+			else {
+
+				printf("ERROR!!! Employee does not exist\n\n");
+			}
+
+			break;
+
+		case 4: // Remove Employee
+				// @AT-HOME
+			printf("Remove Employee\n");
+			printf("===============\n");
+			do {
+
+				printf("Enter Employee ID: ");
+				scanf("%d", &tempEmpID);
+
+				if (tempEmpID <= 0) {
+					printf("ERROR: EmployeeID should be greater than zero !!\n");
+				}
+
+			} while (tempEmpID <= 0);
+
+			//Find the record
+			for (i = 0; i < SIZE && check == -1; i++) {
+				if (emp[i].empID == tempEmpID) {
+					check = i;
+				}
+			}
+
+			// Employee Found
+			if (check >= 0) {
+
+				
+				printf("Employee %d will be removed\n\n", emp[check].empID);
+				emp[check].empID = 0; //Employee ID is made zero now to remove the employee
+				check = -1;
+				empNo--;
+
+			}
+			else {
+
+				printf("ERROR!!! Employee does not exist\n\n");
+			}
+
+			break;
+
+		default:
+			printf("ERROR: Incorrect Option: Try Again\n\n");
+		}
+
+	} while (option != 0);
+
+
+
+	return 0; 
+}
+
+
+// PROGRAM OUTPUT IS SHOW BELOW
+
+/*
+
+---=== EMPLOYEE DATA ===---
+
+1. Display Employee Information
+2. Add Employee
+3. Update Employee Salary
+4. Remove Employee
+0. Exit
+
+Please select from the above options: 5
+
+ERROR: Incorrect Option: Try Again
+
+1. Display Employee Information
+2. Add Employee
+3. Update Employee Salary
+4. Remove Employee
+0. Exit
+
+Please select from the above options: 2
+
+Adding Employee
+===============
+Enter Employee ID: 111
+Enter Employee Age: 34
+Enter Employee Salary: 78980.88
+
+1. Display Employee Information
+2. Add Employee
+3. Update Employee Salary
+4. Remove Employee
+0. Exit
+
+Please select from the above options: 2
+
+Adding Employee
+===============
+Enter Employee ID: 112
+Enter Employee Age: 41
+Enter Employee Salary: 65000
+
+1. Display Employee Information
+2. Add Employee
+3. Update Employee Salary
+4. Remove Employee
+0. Exit
+
+Please select from the above options: 2
+
+Adding Employee
+===============
+Enter Employee ID: 113
+Enter Employee Age: 53
+Enter Employee Salary: 120345.78
+
+1. Display Employee Information
+2. Add Employee
+3. Update Employee Salary
+4. Remove Employee
+0. Exit
+
+Please select from the above options: 2
+
+Adding Employee
+===============
+Enter Employee ID: 114
+Enter Employee Age: 25
+Enter Employee Salary: 46780
+
+1. Display Employee Information
+2. Add Employee
+3. Update Employee Salary
+4. Remove Employee
+0. Exit
+
+Please select from the above options: 2
+
+Adding Employee
+===============
+ERROR!!! Maximum Number of Employees Reached
+
+1. Display Employee Information
+2. Add Employee
+3. Update Employee Salary
+4. Remove Employee
+0. Exit
+
+Please select from the above options: 1
+
+EMP ID  EMP AGE EMP SALARY
+======  ======= ==========
+   111       34   78980.88
+   112       41   65000.00
+   113       53  120345.78
+   114       25   46780.00
+
+1. Display Employee Information
+2. Add Employee
+3. Update Employee Salary
+4. Remove Employee
+0. Exit
+
+Please select from the above options: 3
+
+Update Employee Salary
+======================
+Enter Employee ID: 112
+The current salary is 65000.00
+Enter Employee New Salary: 99999.99
+
+1. Display Employee Information
+2. Add Employee
+3. Update Employee Salary
+4. Remove Employee
+0. Exit
+
+Please select from the above options: 1
+
+EMP ID  EMP AGE EMP SALARY
+======  ======= ==========
+   111       34   78980.88
+   112       41   99999.99
+   113       53  120345.78
+   114       25   46780.00
+
+1. Display Employee Information
+2. Add Employee
+3. Update Employee Salary
+4. Remove Employee
+0. Exit
+
+Please select from the above options: 4
+
+Remove Employee
+===============
+Enter Employee ID: 112
+Employee 112 will be removed
+
+1. Display Employee Information
+2. Add Employee
+3. Update Employee Salary
+4. Remove Employee
+0. Exit
+
+Please select from the above options: 1
+
+EMP ID  EMP AGE EMP SALARY
+======  ======= ==========
+   111       34   78980.88
+   113       53  120345.78
+   114       25   46780.00
+
+1. Display Employee Information
+2. Add Employee
+3. Update Employee Salary
+4. Remove Employee
+0. Exit
+
+Please select from the above options: 0
+
+Exiting Employee Data Program. Good Bye!!!
+
+*/
+
+
